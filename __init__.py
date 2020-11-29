cfbd6812671be0999296a81dc24438919d83e392
Juan <administrator@Juans-MacBook-Air.local>
2020-11-28 19:30:18 -0500

    add nutrients restrictions

--- __init__.py
+++ __init__.py
@@ -1,14 +1,13 @@
 
 import matplotlib.pyplot as plt
 import math
+import numpy as np
 
 INTERVALE_OF_MONTHS = 3
 YEARS_TO_SIMULATE = 5
 
 PERCENTAGE_REDUCTION_WATER_DEFICIT = 0.05
 PERCENTAGE_REDUCTION_OVERDENSITY = 0.60
-
-
 
 def get_months():
     months = []
@@ -41,6 +40,38 @@ def water_requirements(densidad, month):
 
 def get_optimal_density(solar_ligth_year, capacity_save_water, months_sequia):
     return 7429.170455 + (-2.160113636 * solar_ligth_year) + (39.48238636 * capacity_save_water) + (-0.08522727271 * months_sequia)
+
+def estimate_coef(x, y): 
+    # number of observations/points 
+    n = np.size(x) 
+  
+    # mean of x and y vector 
+    m_x, m_y = np.mean(x), np.mean(y) 
+  
+    # calculating cross-deviation and deviation about x 
+    SS_xy = np.sum(y*x) - n*m_y*m_x 
+    SS_xx = np.sum(x*x) - n*m_x*m_x 
+  
+    # calculating regression coefficients 
+    b_1 = SS_xy / SS_xx 
+    b_0 = m_y - b_1*m_x 
+  
+    return (b_0, b_1) 
+
+def nutrients_by_months(mo, month):
+    if mo <= 8:
+        nitrogeno = 5.55 + (0.664 * month) + (-4.46 * (10**-3)) * month**2
+        urea = 12.1 + 1.43 * month + (-8.93 * (10**-3)) * month**2
+        return (nitrogeno, urea)
+    elif  mo > 8:
+        nitrogeno = 3.95 + 0.486 * month + (4.46 * (10**-3)) * month**2
+        urea = 1.25 * month + 7.5
+        return (nitrogeno, urea)
+    else:
+        return (0, 0)
+
+def run_linear(a, b, month): #Y= a + bX
+    return a + b * month
 
 class Simulation:
 
@@ -75,28 +106,64 @@ class Simulation:
             for i in range(0, len(self.predictions)):
                 self.predictions[i] -= self.predictions[i] * percentage_reduction * PERCENTAGE_REDUCTION_OVERDENSITY
 
+    def increase_values(self, i, percentage):
+         for i in range(i, len(self.predictions)):
+                self.predictions[i] += self.predictions[i] * percentage
+    
+    def decrease_values(self, i, percentage):
+         for i in range(i, len(self.predictions)):
+                self.predictions[i] -= self.predictions[i] * percentage
 
+    def apply_nutrients_restrictions(self):
+ 
+        x_n = np.array([2, 6, 10, 14, 18]) 
+        y_n = np.array([self.params['nit_2'], self.params['nit_6'], self.params['nit_10'], self.params['nit_14'], self.params['nit_18']]) 
+
+        x_u = np.array([2, 6, 10, 14, 18]) 
+        y_u = np.array([self.params['urea_2'], self.params['urea_6'], self.params['urea_10'], self.params['urea_14'], self.params['urea_18']]) 
+
+        nitrogeno = estimate_coef(x_n, y_n) 
+        urea = estimate_coef(x_u, y_u)
+
+        for i in range(0, len(self.predictions)):
+            month = self.months[i] 
+            (ideal_nit, ideal_ur) = nutrients_by_months(self.params['mo'], month)
+            current_nit = run_linear(nitrogeno[0], nitrogeno[1], month)
+            current_ur = run_linear(urea[0], urea[1], month)
+
+
+            if current_nit > ideal_nit or current_ur > ideal_ur:
+                self.increase_values(i, 0.02)
+            else:
+                self.decrease_values(i, 0.025)
 
     def start(self):
         
         self.apply_water_restrictions()
         self.apply_density_restrictions()
+        self.apply_nutrients_restrictions()
 
         return self.predictions
-
-
-
-
 
 if __name__ == '__main__':
     params = {
         'densidad': 7500,
         'cad': 57,
         'meses_cons_secos': 1,
-        'horas_brillo_solar': 1400
+        'horas_brillo_solar': 1400,
+        'mo': 8,
+        'nit_2': 7,
+        'nit_6': 9,
+        'nit_10': 12,
+        'nit_14': 14,
+        'nit_18': 16,
+
+        'urea_2': 15,
+        'urea_6': 20,
+        'urea_10': 26,
+        'urea_14': 30,
+        'urea_18': 35
     }
-    print(water_requirements(params['densidad'], 38))
-    
     simulation = Simulation(params)
 
     predictions = simulation.start()
@@ -106,4 +173,3 @@ if __name__ == '__main__':
     plt.ylabel('Altura (cm)') 
     plt.xlabel('Tiempo (meses)')
     plt.show()
-    #get_baseline()
